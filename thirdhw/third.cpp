982196caeb1663888a8f5b44f317934fc286525c
devKmo <kutayozkocgithub.com>
2025-08-12 01:15:34 +0300

    odevverildi

---
+++ thirdhw/third.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/*
+
+Kullanýcýya bir arayüz yapacaðýz.
+
+Yapýlacak iþlemi seçiniz...
+
+1.	Toplama
+2.	Çýkarma
+3.	Çarpma
+4.	Bölme
+
+(if else öðrenilecek.) (Yapabilirsen switch case öðrencen)
+
+if secilen sayi = 4 ise bölme yapicak.
+
+4 seçimini yaptiniz.
+
+Birinci sayiyi giriniz.
+ikinci sayiyi giriniz.
+
+bolme = Sonuç
+
+*/
+
+int main() {
+
+	return 0;
+}
