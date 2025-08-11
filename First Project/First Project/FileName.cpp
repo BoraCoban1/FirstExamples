d391ed6f8793d9bc627c23b9e94b1295ebea7d6b
BoraCoban1 <boracoban143@gmail.com>
2025-08-11 23:59:08 +0300

    first commit

---
+++ First Project/First Project/FileName.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+	int num1 = 10;
+	int num2 = 20;
+	int toplam;
+	int carpim;
+	
+	toplam = num1 + num2;
+
+	carpim = num1 * num2;
+	
+	cout << "carpim =" << carpim << endl;
+
+	
+
+	cout << "PROSPERO" << endl;
+
+ 
+
+	return 0;
+}
---
+++ odev/odev/odev.cpp
@@ -0,0 +1,57 @@
+#include <iostream> <string>
+
+using namespace std;
+
+
+int main() {
+
+	int num1 = 10;
+	int num2 = 20;
+	int yas = 17;
+	int toplam;
+	int cikarma;
+	int carpim;
+	int bolum;
+	string isim = "Bora";
+	string soyisim = "Coban";
+
+
+
+
+
+	toplam = num1 + num2;
+
+	cikarma = num2 - num1;
+
+	carpim = num1 * num2;
+
+	bolum = num2 / num1;
+
+	cout << "Ben" << endl;
+
+	cout << endl;
+
+	cout << isim << endl;
+
+	cout << soyisim << endl;
+
+
+
+	cout << yas << " " <<  "YASINDAYIM" << endl;
+
+	cout << "toplam =" << toplam << endl;
+
+	cout << "cikarma =" << cikarma << endl;
+
+	cout << "carpim =" << carpim << endl;
+	
+	cout << "bolum =" << bolum << endl;
+	
+	return 0;
+
+
+
+
+
+
+}
