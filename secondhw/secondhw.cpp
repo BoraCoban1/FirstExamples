ec63095d6e33b85fb6e30c12fca2d72ae996739a
devKmo <kutayozkocgithub.com>
2025-08-12 00:24:28 +0300

    odevverildi

---
+++ secondhw/secondhw.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+	Kullanýcýdan isim soy isim alacaðýz.
+	Yaþýný Öðreneceðiz. 
+	Ýþlem yapabilmesi için iki tane sayý alacaðýz.
+	
+
+	En son örnek bir çýktý üreteceðiz.
+
+	Ben
+
+	Bora
+	Coban
+	17 Yasindayim
+	Toplam = X+Y
+	Carpma = X+Y
+	Cikarma = X+Y
+	Bolme = X+Y
+*/
+
+int main() {
+
+
+
+	return 0;
+}
